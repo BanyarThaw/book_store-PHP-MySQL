1448b58eee7cbc1dcf1cba33ffb273e411e7476d
BanyarThaw <banyarthaw2@gmail.com>
2023-12-01 16:44:03 +0630

    Message: Testing Jenkins Build

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Book Store</title>
+  <title>Book Store Update</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/pagination.css">
 </head>
